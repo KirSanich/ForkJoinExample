723291b57e9e3ab14a758eeb4ea6688fd07a68d9
Kirill Sokolov <aqua_agera_ls4@mail.ru>
2021-12-06 20:42:01 +0300

    pattern builder

---
+++ src/com/company/Employee.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Employee extends Main.Person.Builder {
+    public Employee(String name, int age) {
+        super(name, age);
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,52 +1,100 @@
 package com.company;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
-import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
-        ForkJoinPool forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
-        int[] mas = {5, 7, 8, 9, 0, 11, 55};
-        FindMaxTask findMaxTask = new FindMaxTask(mas, 1, 7);
-        Integer res = forkJoinPool.invoke(findMaxTask);
-        System.out.println(res);
+        Person person = new Person.Builder("Kirill", 21).height(180).weight(69).build();
+        var stream = ThreadLocalRandom.current().ints().limit(20);
+        stream.forEachOrdered(System.out::println);
+
+        Employee employee = Person.valueOf();
 
 
     }
 
 
-    static class FindMaxTask extends RecursiveTask<Integer> {
-        private int[] array;
-        private int start, end;
+    static class Holder {
+        private int x;
 
-        public FindMaxTask(int[] array, int start, int end) {
-            this.array = array;
-            this.start = start;
-            this.end = end;
+        public Holder(int x) {
+            this.x = x;
+        }
+
+        public static Holder valueOf(int x) {
+            return new Holder(x);
+        }
+    }
+
+
+    static class Person {
+
+        private String name;
+        private int age;
+        private int height;
+        private int weight;
+
+        static class Builder {
+            private String name;
+            private int age;
+            private int height = 0;
+            private int weight = 0;
+
+            public Builder(String name, int age) {
+                this.name = name;
+                this.age = age;
+            }
+
+            public Builder height(int value) {
+                height = value;
+                return this;
+            }
+
+            public Builder weight(int value) {
+                weight = value;
+                return this;
+            }
+
+            public Person build() {
+                return new Person(this);
+            }
+
+        }
+
+        private Person(Builder builder) {
+            name = builder.name;
+            age = builder.age;
+            weight = builder.weight;
+            height = builder.height;
+        }
+
+        public static Employee valueOf() {
+            return new Employee("lr",21);
         }
 
         @Override
-        protected Integer compute() {
-            if (end - start <= 4) {
-                List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
-                Optional<Integer> optional = list.stream().max(Comparator.naturalOrder());
-                return optional.orElse(-100);
-            } else {
-                int mid = (end - start) / 2 + start;
-                FindMaxTask left = new FindMaxTask(array, start, mid);
-                FindMaxTask right = new FindMaxTask(array, mid + 1, end);
-                ForkJoinTask.invokeAll(right, left);
-                int leftRes = left.getRawResult();
-                int rightRes = right.getRawResult();
-                return Integer.max(leftRes, rightRes);
-            }
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return age == person.age && height == person.height && weight == person.weight && name.equals(person.name);
         }
 
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age, height, weight);
+        }
     }
+
+
 }
+
+
+
+
